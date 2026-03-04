88aa06eebc7437286dbe945b8a82474c1377a188
IngenieroRicardo <138132387+IngenieroRicardo@users.noreply.github.com>
2026-03-04 11:17:21 -0600

    Add files via upload

---
+++ ejemplos/web assembly/HTTPautenticacion.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+    "time"
+    http "github.com/IngenieroRicardo/http/go"
+)
+
+func token_handler(req http.HttpRequest) http.HttpResponse {
+    if (http.ValidateToken(req.GetBearerToken())) {
+        return http.CreateResponse(200, `{"message":"Valid token"}`)
+    } else {
+        return http.CreateResponse(403, `{"error":"Invalid token"}`)
+    }
+}
+
+func login_handler(req http.HttpRequest) http.HttpResponse {
+    if  req.GetUsername() == "admin" && req.GetPassword() == "secret" {
+        return http.CreateResponse(200, http.GenerateToken(1, 60))
+    } else {
+    	return http.CreateResponse(401, `{"error":"Invalid credentials"}`)
+    }
+}
+
+func main() {
+    http.RegisterHandler("/api/login", login_handler)
+    http.RegisterHandler("/api/protected", token_handler)
+    http.StartServer("8080", 0, "", "")
+    
+    for {
+        time.Sleep(1 * time.Second)
+    }
+}
+
+/* PRUEBA DESDE LA CONSOLA DEL NAVEGADOR:
+
+fetch('/api/login', {
+    method: 'POST',
+    headers: {
+        'Authorization': 'Basic ' + btoa('admin:secret'),
+        'User-Agent': 'Navegor'
+    }
+})
+.then(res => res.text())
+.then(alert);
+
+
+
+fetch('/api/protected', {
+    method: 'POST',
+    headers: {
+        'Authorization': 'Bearer 12345',
+        'User-Agent': 'Navegor'
+    }
+})
+.then(res => res.text())
+.then(alert);
+
+*/
